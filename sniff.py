6a8cbcd5d8d45833a3467aecc81d5eae8e90ba52
Samuel Wehunt <sam.wehunt@gmail.com>
2017-10-14 17:36:38 -0500

    trying to find the right filter...

--- sniff.py
+++ sniff.py
@@ -4,4 +4,4 @@ def pkt_callback(packet):
     packet.show()
 
 interface = "ens10"
-sniff(iface=interface, prn=pkt_callback, filter="ip proto tcp", store=0)
+sniff(iface=interface, prn=pkt_callback, filter="tcp", store=0)
