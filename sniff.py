5ec1e0dea02fbc9ae267222aaa964778c95c5a1c
Samuel Wehunt <sam.wehunt@gmail.com>
2017-10-14 17:38:29 -0500

    trying to find the right filter...

--- sniff.py
+++ sniff.py
@@ -1,7 +1,8 @@
 from scapy.all import *
 
 def pkt_callback(packet):
-    packet.show()
+    if (packet[TCP]):
+        packet.show()
 
 interface = "ens10"
 sniff(iface=interface, prn=pkt_callback, filter="tcp", store=0)
