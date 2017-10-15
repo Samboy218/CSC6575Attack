9a4aa123ff07e214bf8a6f1e6102edb8e538e0b8
Samuel Wehunt <sam.wehunt@gmail.com>
2017-10-14 21:48:21 -0500

    added reset function

--- rst.py
+++ rst.py
@@ -5,7 +5,7 @@ target_ip = "192.168.13.30"
 
 def pkt_callback(packet):
     if (TCP in packet):
-        print("seq: %d, ack: %d\n" % packet[TCP].seq, packet[TCP].ack)
+        print("seq: %d, ack: %d\n" % (packet[TCP].seq, packet[TCP].ack))
         if (packet[IP].dst == ""):
             resetPkt = packet
             #set reset packet
